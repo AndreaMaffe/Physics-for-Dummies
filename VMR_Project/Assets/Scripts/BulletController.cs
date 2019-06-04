21c2c410efac3b62d3097927b313c195ad27fec9
Federico <cereale.federico@gmail.com>
2019-06-05 00:13:46 +0200

    FedeScene physics consistent structure added

---
+++ VMR_Project/Assets/Scripts/BulletController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject velocity;
+    public GameObject velocityX;
+    public GameObject velocityY;
+    public GameObject gravity;
+
+    private float factorScale = 0.15f;
+    private Vector3 lastFramePosition;
+    private Vector3 initialPosition;
+    private Vector velocityArrowScript;
+    private Vector velocityXArrowScript;
+    private Vector velocityYArrowScript;
+
+    void Start() {
+        gravity.GetComponent<Vector>().SetScale(9.81f * factorScale);
+        velocityArrowScript = velocity.GetComponent<Vector>();
+        velocityXArrowScript = velocityX.GetComponent<Vector>();
+        velocityYArrowScript = velocityY.GetComponent<Vector>();
+        lastFramePosition = transform.position;
+        initialPosition = transform.position;
+    }
+    
+    void FixedUpdate()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 linearVelocity = transform.position - lastFramePosition;
+        Vector3 newRotation = Quaternion.LookRotation(linearVelocity).eulerAngles + Quaternion.Euler(90f, 0f, 0f).eulerAngles;
+        float alphaRad = (newRotation.x - 90) * Mathf.PI / 180;
+        Vector3 newPosition = new Vector3(0f, -Mathf.Sin(alphaRad), Mathf.Cos(alphaRad));
+        float vel = rb.velocity.magnitude;
+        bool isDescending = -Mathf.Sin(alphaRad) < 0;
+
+        velocity.transform.rotation = Quaternion.Euler(newRotation);
+        velocityArrowScript.SetScale(vel * factorScale);
+        velocityXArrowScript.SetScale(vel * Mathf.Cos(alphaRad) * factorScale);
+        velocityY.transform.localRotation = Quaternion.Euler(!isDescending ? 0f : 180f, 0f, 0f);
+        velocityYArrowScript.SetScale(vel * Mathf.Abs(-Mathf.Sin(alphaRad)) * factorScale);
+        //gravity.transform.rotation = Quaternion.Euler(180, 0, 0);
+
+        lastFramePosition = transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            bullet.SetActive(false);
+            velocity.SetActive(false);
+            velocityX.SetActive(false);
+            velocityY.SetActive(false);
+            gravity.SetActive(false);
+            StartCoroutine("DestroyCannonBall");
+        }
+    }
+
+    IEnumerator DestroyCannonBall()
+    {
+        TrailRenderer trail = GetComponent<TrailRenderer>();
+        yield return new WaitUntil(() => trail.positionCount==0);
+        Destroy(gameObject);
+        yield return null;
+    }
+
+}
---
+++ VMR_Project/Assets/Scripts/C.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/* ------------------------------------------------------------------------------------------------
+
+    FEDERICO:
+    Ho aggiunto questo script perchè nella mia scena il cannone ruotava ma lo faceva anche la base 
+    (quella marrone per intenderci).
+    Non ho voluto toccare lo script che usi per evitare conflitti o cose inaspettate.
+    Qui ho aggiunto il gameobject del cannone stesso e ho modificato alcune righe cosi da evitare il problema.
+    Domani in caso ti spiego meglio cosa intendo e poi vedi tu cosa fare.
+     
+--------------------------------------------------------------------------------------------------*/
+
+public class C : MonoBehaviour
+{
+    public float angularSpeed = 20f;
+    public float cannonPower = 60f;
+    public GameObject cannonBody;
+    public Transform cannonSphere;
+    public Transform shootingPoint;
+    public GameObject bullet;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            MoveUp();
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            MoveDown();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    void MoveUp()
+    {
+        //if ((transform.localRotation.eulerAngles.x <= 360f && transform.localRotation.eulerAngles.x >= 300f) || (transform.localRotation.eulerAngles.x >= 0f && transform.localRotation.eulerAngles.x <= 46f)) {
+        cannonBody.transform.RotateAround(cannonSphere.transform.position, Vector3.Cross(-transform.forward, transform.up), -angularSpeed * Time.deltaTime);
+        //}
+    }
+
+    void MoveDown()
+    {
+        //if ((transform.localRotation.eulerAngles.x >= -1f && transform.localRotation.eulerAngles.x <= 45f) || (transform.localRotation.eulerAngles.x >= 299f && transform.localRotation.eulerAngles.x <= 360f)) {
+        cannonBody.transform.RotateAround(cannonSphere.transform.position, Vector3.Cross(-transform.forward, transform.up), angularSpeed * Time.deltaTime);
+        //}
+    }
+
+    void Fire()
+    {
+        GameObject bulletObject = Instantiate(bullet, shootingPoint.position, transform.rotation);
+        Rigidbody bulletRb = bulletObject.GetComponent<Rigidbody>();
+        bulletRb.AddForce(shootingPoint.forward * cannonPower, ForceMode.VelocityChange);
+        AudioManager.instance.PlaySound(SoundType.Boom);
+
+    }
+}
