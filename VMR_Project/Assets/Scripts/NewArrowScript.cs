e43bab48b3630d075fb37948b212b1344d90b118
Federico <cereale.federico@gmail.com>
2019-06-04 17:25:42 +0200

    new arrow model and related script

---
+++ VMR_Project/Assets/Scripts/NewArrowScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewArrowScript : MonoBehaviour
+{
+
+    public GameObject trail;
+    public GameObject point;
+
+    float vectorScale = 1f;
+
+    public void SetScale(float scale)
+    {
+        vectorScale = scale;
+        trail.transform.localScale = new Vector3(1f, vectorScale, 1f);
+        float newY = trail.transform.localScale.y * 2 + point.transform.localScale.y / 2;
+        point.transform.localPosition = new Vector3(0f, newY, 0f);
+    }
+}
