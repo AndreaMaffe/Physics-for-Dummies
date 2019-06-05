c3dd165f2b862783ba85f5a8ee05ba43fe5ead8f
Luca Cellamare <luca.g.cellamare@gmail.com>
2019-06-05 14:21:53 +0200

    update LucaScene

---
+++ VMR_Project/Assets/Scripts/forza_applicata.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class applyforce : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public Vector3 direction;
+    public float amount;
+    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(direction*amount, ForceMode.Impulse);
+    }
+
+
+
+
+}
