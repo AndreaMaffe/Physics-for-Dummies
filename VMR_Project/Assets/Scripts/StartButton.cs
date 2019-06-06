3a49f31cc0c94b605d6d578a75c4b78912cfee00
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2019-06-06 11:10:38 +0200

    StartButton Asset

---
+++ VMR_Project/Assets/Scripts/StartButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : InteractiveObject
+{
+    public Material offMaterial;
+    public Material onMaterial;
+    public Animator animator;
+
+    private bool isOn;
+    private MeshRenderer meshRenderer;
+
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        isOn = false;
+    }
+
+    protected override void OnArrowDown()
+    {
+    }
+
+    protected override void OnArrowUp()
+    {
+    }
+
+    protected override void OnClick()
+    {
+        animator.SetTrigger("Pressed");
+
+        if (isOn)
+        {
+            isOn = false;
+            meshRenderer.material = offMaterial;
+        }
+
+        else
+        {
+            isOn = true;
+            meshRenderer.material = onMaterial;
+        }
+    }
+
+
+}
