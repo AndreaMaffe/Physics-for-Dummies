963649a45889eb1acf38bfbb0f3ccd81abb6ff3f
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2019-06-06 23:02:36 +0200

    Division between TitleScreen, MainMenu & Experiences

---
+++ VMR_Project/Assets/Scripts/GreenButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenButton : InteractiveObject
+{
+    public Animator animator;
+    public int index;
+
+    public override void OnFocusEnter()
+    {
+        Debug.Log(gameObject.name + "focused!");
+        focused = true;
+        animator.SetBool("Focused", true);
+    }
+
+    public override void OnFocusExit()
+    {
+        Debug.Log(gameObject.name + "stop focused!");
+        focused = false;
+        animator.SetBool("Focused", false);
+    }
+
+    public override void OnArrowDown()
+    {
+    }
+
+    public override void OnArrowUp()
+    {
+    }
+
+    public override void OnClick()
+    {
+
+    }
+}
