079873a0081b716bd1a811dd5ae3cc188c9da692
Paolo Boffi <37587240+Paul995@users.noreply.github.com>
2019-06-06 14:22:18 +0200

    Incline update

---
+++ VMR_Project/Assets/Scripts/Paolo/Resources/FrictionButton.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrictionButton : InteractiveObject
+{
+    public Material lowMaterial;
+    public Material middleMaterial;
+    public Material highMaterial;
+    public PhysicMaterial ice;
+    public PhysicMaterial wood;
+    public PhysicMaterial steel;
+    public Animator animator;
+    public PaoloManager manager;
+
+    private PhysicMaterial[] physicMaterials = new PhysicMaterial[3];
+    private Material[] materials = new Material[3];
+    private int isOn;
+    private MeshRenderer meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        materials[0] = lowMaterial;
+        materials[1] = middleMaterial;
+        materials[2] = highMaterial;
+        physicMaterials[0] = ice;
+        physicMaterials[1] = wood;
+        physicMaterials[2] = steel;
+        meshRenderer = GetComponent<MeshRenderer>();
+        isOn = 0;
+        meshRenderer.material = materials[isOn];
+    }
+
+    protected override void OnArrowUp()
+    {
+        animator.SetTrigger("Pressed");
+        
+        if(isOn == 2)
+        {
+            isOn = 0;
+        } else
+        {
+            isOn++;
+        }
+        meshRenderer.material = materials[isOn];
+        manager.ramp.incline.GetComponent<BoxCollider>().material = physicMaterials[isOn];
+    }
+
+    protected override void OnArrowDown()
+    { 
+        animator.SetTrigger("Pressed");
+
+        if (isOn == 0)
+        {
+            isOn = 2;
+        }
+        else
+        {
+            isOn--;
+        }
+        meshRenderer.material = materials[isOn];
+        manager.ramp.incline.GetComponent<BoxCollider>().material = physicMaterials[isOn];
+    }
+
+    protected override void OnClick()
+    {
+    }
+}
