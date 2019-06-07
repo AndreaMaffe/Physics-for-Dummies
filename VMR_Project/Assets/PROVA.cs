d265222d48de4f8fbc7b695ae0f648720057db7d
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2019-06-07 14:59:26 +0200

    new Assets

---
+++ VMR_Project/Assets/PROVA.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PROVA : MonoBehaviour
+{
+    public GameObject cube;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            cube.GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            cube.GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, 1);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(0, 0, -1);
+        }
+    }
+}
