a6d3c41dc202ba3ad420e33e0716b54fa1cd1cd6
Luca Cellamare <luca.g.cellamare@gmail.com>
2019-06-07 15:08:26 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ VMR_Project/Assets/Scripts/ShockSphere.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockSphere : InteractiveObject
+{
+    private Rigidbody rb;
+    private float scale;
+    public float amount;
+    public Transform otherSphere;
+
+    private Vector3 originalPosition;
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+        originalPosition = transform.position;
+        scale = 1f;
+        Vector3 direction = otherSphere.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+
+    public override void OnArrowDown()
+    {
+
+        scale -= 0.2f;
+
+        if (scale < 0.4f)
+            scale = 0.4f;
+
+        this.transform.localScale = new Vector3(scale, scale, scale);
+
+        rb.mass -= 1f;
+
+        if (rb.mass < 10f)
+            rb.mass = 10f;
+
+    }
+
+    public override void OnArrowUp()
+    {
+        scale += 0.2f;
+
+        if (scale > 1.8f)
+            scale = 1.8f;
+
+        this.transform.localScale = new Vector3(scale, scale, scale);
+
+        rb.mass += 1f;
+
+        if (rb.mass > 30f)
+            rb.mass = 30f;
+    }
+
+
+
+    public void ResetPosition()
+    {
+        rb.angularVelocity = Vector3.zero;
+        rb.velocity = Vector3.zero;
+        transform.position = originalPosition;
+
+    }
+
+    public void StartAddForce()
+    {
+        rb.AddForce(transform.forward * amount, ForceMode.VelocityChange);
+    }
+
+    public override void OnClick()
+    {
+    }
+}
---
+++ VMR_Project/Assets/Scripts/StartShockButton.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartShockButton : InteractiveObject
+{
+
+    public Material offMaterial;
+    public Material onMaterial;
+    public Animator animator;
+
+    private bool isOn;
+    private MeshRenderer meshRenderer;
+    public ShockSphere shockSphere1;
+    public ShockSphere shockSphere2;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        isOn = false;
+    }
+
+    public override void OnArrowDown()
+    {
+    }
+
+    public override void OnArrowUp()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            OnClick();
+            
+    }
+
+    public override void OnClick()
+    {
+        //if (focused)
+        {
+            animator.SetTrigger("Pressed");
+            AudioManager.instance.PlaySound(SoundType.Pop);
+
+            if (isOn)
+            {
+                isOn = false;
+                meshRenderer.material = offMaterial;
+                shockSphere1.ResetPosition();
+                shockSphere2.ResetPosition();
+            }
+
+            else
+            {
+                isOn = true;
+                meshRenderer.material = onMaterial;
+                shockSphere1.StartAddForce();
+            }
+        }
+    }
+}
